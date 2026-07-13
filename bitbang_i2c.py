3a73da20977e27f094d4fcdbdbd1fd12904afc8a
irih01 <199847844+irih01@users.noreply.github.com>
2026-07-13 08:52:25 +0300

    Update bitbang_i2c.py

--- bitbang_i2c.py
+++ bitbang_i2c.py
@@ -1,28 +1,28 @@
-import time
-import nidaqmx
-from nidaqmx.constants import LineGrouping, TaskMode
-from ctypes import c_ubyte
-
 class DigitalPin:
     def __init__(self, channel):
         self.channel = channel
+        # Faci un singur task de scriere, dar îl configurezi corect de la început
         self.write_task = nidaqmx.Task()
         self.read_task = nidaqmx.Task()
         
-       
-        self.write_task.do_channels.add_do_chan(self.channel,line_grouping=LineGrouping.CHAN_PER_LINE)
-        self.read_task.di_channels.add_di_chan(self.channel,line_grouping=LineGrouping.CHAN_PER_LINE)
+        self.write_task.do_channels.add_do_chan(self.channel, line_grouping=LineGrouping.CHAN_PER_LINE)
+        self.read_task.di_channels.add_di_chan(self.channel, line_grouping=LineGrouping.CHAN_PER_LINE)
+        
+        # Le pornești o singură dată AICI, la inițializare, nu la fiecare bit!
+        self.write_task.start()
+        self.read_task.start()
 
     def set_output(self):
-        self.write_task.start()
+        # În loc să oprești task-uri, doar scrii valoarea 0 ca să tragi linia în LOW
+        self.write(0)
 
     def set_input(self):
-        self.write_task.stop()
-        self.read_task.start()
+        # Pentru I2C pseudo-open-drain, scrii 1 ca să lași pull-up-ul extern să ridice linia
+        self.write(1)
     
     def write(self, value):
         data = (c_ubyte * 1)(value)
-        self.write_task.write(1,data=data)
+        self.write_task.write(1, data=data)
 
     def read(self):
         data = (c_ubyte * 1)()
@@ -30,92 +30,8 @@ class DigitalPin:
         return data[0]
     
     def close(self):
+        # Le oprești și le cureți doar când închizi tot scriptul, la final
         self.write_task.stop()
         self.write_task.close()
         self.read_task.stop()
         self.read_task.close()
-
-
-class BitBangI2C:
-    def __init__(self, scl_pin, sda_pin, delay=0.001):
-        self.scl = DigitalPin(scl_pin)
-        self.sda = DigitalPin(sda_pin)
-        self.delay = delay
-
-    def sleep(self):
-        time.sleep(self.delay)
-
-    def scl_high(self):
-        self.scl.set_input()
-        self.sleep()
-
-    def scl_low(self):
-        self.scl.set_output()
-        self.scl.write(0)
-        self.sleep()
-
-    def sda_high(self):
-        self.sda.set_input()
-        self.sleep()
-
-    def sda_low(self):
-        self.sda.set_output()
-        self.sda.write(0)
-        self.sleep()
-
-    def start(self):
-        self.sda_high()
-        self.scl_high()
-        self.sleep()
-        self.sda_low()
-        self.sleep()
-        self.scl_low()
-        self.sleep()
-
-    def stop(self):
-        self.sda_low()
-        self.scl_high()
-        self.sleep()
-        self.sda_high()
-        self.sleep()
-
-    def write_byte(self, byte):
-        for i in range(8):
-            if (byte & 0x80):
-                self.sda_high()
-            else:
-                self.sda_low()
-            byte <<= 1
-            self.scl_high()
-            self.scl_low()
-
-            #ACK bit
-        self.sda_high() #release SDA
-        self.scl_high()
-        ack = self.sda.read()
-        self.scl_low()
-        return ack == 0
-    
-    def read_byte(self, ack=True):
-        byte = 0
-        self.sda_high()
-        for _ in range(8):
-            self.scl_high()
-            bit = self.sda.read()
-            byte = (byte << 1) | bit
-            self.scl_low()
-        # Send ACK/NACK
-        if ack:
-            self.sda_low()
-        else:
-            self.sda_high()
-        self.scl_high()
-        self.scl_low()
-        self.sda_high()
-        return byte
-    
-    def close(self):
-        self.scl.close()
-        self.sda.close()
-        
-        
